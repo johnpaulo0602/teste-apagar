6bae0a98634b4c5b43b0003016bbc2a2420330ae
bernar-seazone <b.duarte@seazone.com.br>
2024-12-16 15:54:46 -0300

    chore: added configs to terraform

--- terraformApps/productionApps/reservas/main.tf
+++ terraformApps/productionApps/reservas/main.tf
@@ -47,3 +47,152 @@ resource "aws_ecr_lifecycle_policy" "this" {
 }
 EOF
 }
+
+resource "aws_s3_bucket" "seo" {
+  bucket = var.bucket_name_seo
+
+  tags = {
+    Name        = var.bucket_name_seo
+    Environment = var.environment
+    Projeto = var.project
+  }
+}
+
+resource "aws_s3_bucket" "ranking" {
+  bucket = var.bucket_name_ranking
+
+  tags = {
+    Name        = var.bucket_name_ranking
+    Environment = var.environment
+    Projeto = var.project
+  }
+}
+
+resource "aws_s3_bucket" "hotels" {
+  bucket = var.bucket_name_hotels
+
+  tags = {
+    Name        = var.bucket_name_hotels
+    Environment = var.environment
+    Projeto = var.project
+  }
+}
+
+resource "aws_s3_bucket" "acommodations" {
+  bucket = var.bucket_name_acomodations
+
+  tags = {
+    Name        = var.bucket_name_acomodations
+    Environment = var.environment
+    Projeto = var.project
+  }
+}
+
+data "aws_iam_openid_connect_provider" "this" {
+  arn = "arn:aws:iam::711387131913:oidc-provider/oidc.eks.us-west-2.amazonaws.com/id/88953DFB37911EA9F674AF009D5FC20C"
+}
+
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [data.aws_iam_openid_connect_provider.this.arn]
+    }
+    condition {
+      test     = "ForAnyValue:StringEquals"
+      variable = format( "%s:sub" ,data.aws_iam_openid_connect_provider.this.url)
+      values   = ["system:serviceaccount:apps:reservas"]
+    }
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+  }
+  depends_on = [ data.aws_iam_openid_connect_provider.this ]
+}
+
+resource "aws_iam_policy" "s3" {
+  name = "seazoneReservas"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = [
+			    "s3:PutObject",
+                "s3:GetObject",
+                "s3:ListBucket",
+                "s3:DeleteObject",
+                "s3:GetObjectTagging",
+                "s3:PutObjectTagging"
+				]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role" "this" {
+
+  name        = "reservas"
+  assume_role_policy   = join("", data.aws_iam_policy_document.this.*.json)
+  managed_policy_arns = [aws_iam_policy.s3.arn,"arn:aws:iam::aws:policy/AmazonSSMFullAccess"]
+}
+
+resource "aws_ses_domain_identity" "this" {
+  domain = var.domain
+}
+
+resource "aws_ses_domain_mail_from" "this" {
+  domain           = aws_ses_domain_identity.this.domain
+  mail_from_domain = "no-reply-web-stg.${aws_ses_domain_identity.this.domain}"
+}
+
+# module "db" {
+#   source = "terraform-aws-modules/rds/aws"
+
+#   identifier = var.identifier
+
+#   engine            = var.engine
+#   engine_version    = var.engine_version
+#   instance_class    = var.instance_class
+#   allocated_storage = var.allocated_storage
+#   max_allocated_storage = var.max_allocated_storage
+#   storage_type = var.storage_type
+#   db_name  = var.db_name
+#   username = var.username
+#   port     = var.port
+
+#   iam_database_authentication_enabled = var.iam_database_authentication_enabled
+
+#   vpc_security_group_ids = var.vpc_security_group_ids
+
+#   maintenance_window = var.maintenance_window
+#   backup_window      = var.backup_window
+
+#   # Enhanced Monitoring - see example for details on how to create the role
+#   # by yourself, in case you don't want to create it automatically
+#   monitoring_interval    = var.monitoring_interval
+#   monitoring_role_name   = var.identifier
+#   create_monitoring_role = var.create_monitoring_role
+#   performance_insights_enabled          = var.performance_insights_enabled
+#   performance_insights_retention_period = var.performance_insights_retention_period
+
+#   tags = var.DEFAULT_TAGS
+
+#   # DB subnet group
+#   create_db_subnet_group = var.create_db_subnet_group
+#   subnet_ids             = var.subnets_ids
+#   publicly_accessible = var.publicly_accessible
+#   # Database Deletion Protection
+#   backup_retention_period = var.backup_retention_period
+#   deletion_protection = var.deletion_protection
+
+#   create_db_option_group    = var.create_db_option_group
+#   family = var.family
+#   db_subnet_group_name = var.identifier
+#   create_db_parameter_group = var.create_db_parameter_group
+#   parameter_group_name = var.identifier
+
+# }
--- terraformApps/productionApps/reservas/variables.tf
+++ terraformApps/productionApps/reservas/variables.tf
@@ -1,3 +1,199 @@
 variable "app_name" {
-  default = "wallet-bff"
+  default = "seazone-reservas-api"
 }
+
+variable "dns_name" {
+  default= "api-test"
+}
+
+variable "zone_id" {
+  default = "03880d975c4d65e1cbdddad741e23e33"
+}
+
+variable "bucket_name_ranking" {
+  default = "seazone-reservas-properties-ranking-prd"
+}
+
+variable "bucket_name_seo" {
+  default = "seazone-reservas-seo-page-info-prd"
+}
+
+variable "bucket_name_hotels" {
+  default = "seazone-reservas-google-hotels-properties-feed-prd"
+}
+
+variable "bucket_name_acomodations" {
+  default = "seazone-reservas-referral-acommodations-prd"
+}
+
+variable "project" {
+  default = "reservas"
+}
+
+variable "environment" {
+  default = "production"
+}
+
+variable "domain" {
+  default = "seazone.com.br"
+}
+
+variable "AWS_REGION" {
+  default = "us-west-2"
+}
+
+variable "DEFAULT_TAGS" {
+  type = map(any)
+  default = {
+    environment = "production"
+    project     = "rds-reservas-production"
+    terraform   = "true"
+  }
+}
+
+variable "identifier" {
+  description = "The name of the RDS instance"
+  default     = "rds-reservas-production"
+}
+
+variable "instance_class" {
+  description = "The instance type of the RDS instance"
+  default     = "db.t3.small"
+}
+
+variable "engine" {
+  description = "The database engine to use"
+  default     = "postgres"
+}
+
+variable "engine_version" {
+  description = "The database engine to use"
+  default     = "15"
+}
+
+variable "storage_type" {
+  description = "The database engine to use"
+  default     = "gp3"
+}
+
+variable "subnets_ids" {
+  default     = [
+                  "subnet-0260750d7357e47f3",
+                  "subnet-025b1cea8cdb82c3c",
+                  "subnet-015c34cf458c345fe"
+                ]
+  type        = list(string)
+  description = "The subnet ids that the cluster will use"
+}
+
+
+variable "port" {
+  description = "The port on which the DB accepts connections"
+  default     = 5432
+}
+
+variable "allocated_storage" {
+  description = "The allocated storage in gigabytes"
+  default     = 20
+}
+
+variable "max_allocated_storage" {
+  description = "The allocated storage in gigabytes"
+  default     = 100
+}
+
+variable "db_name" {
+  description = "The DB name to create. If omitted, no database is created initially"
+  default     = "postgres"
+}
+
+variable "username" {
+  description = "Username for the master DB user"
+  default     = "postgres"
+}
+
+variable "iam_database_authentication_enabled" {
+  description = "iam_database_authentication_enabled"
+  default     = false
+  type = bool
+}
+
+variable "create_monitoring_role" {
+  description = "create_monitoring_role"
+  default     = true
+  type = bool
+}
+
+variable "performance_insights_enabled" {
+  description = "performance_insights_enabled"
+  default = true
+  type = bool
+}
+
+variable "publicly_accessible" {
+  description = "Bool to control if instance is publicly accessible"
+  default     = true
+  type = bool
+}
+
+variable "deletion_protection" {
+  description = "The database can't be deleted when this value is set to true"
+  default     = true
+  type = bool
+}
+
+variable "create_db_subnet_group" {
+  description = "Whether to create a database subnet group"
+  default     = true
+  type = bool
+}
+
+variable "vpc_security_group_ids" {
+  description = "List of VPC security groups to associate"
+  default     = ["sg-05a6c2ee9ecf37e19"]
+  type = list(string)
+}
+
+variable "maintenance_window" {
+  description = "The window to perform maintenance in. Syntax: 'ddd:hh24:mi-ddd:hh24:mi'. Eg: 'Mon:00:00-Mon:03:00'"
+  default     = "Mon:00:00-Mon:03:00"
+  type = string
+}
+
+variable "backup_window" {
+  description = "The daily time range (in UTC) during which automated backups are created if they are enabled. Example: '09:46-10:16'. Must not overlap with maintenance_window"
+  default     = "03:00-06:00"
+  type = string
+}
+variable "monitoring_interval" {
+  description = "The ARN for the IAM role that permits RDS to send enhanced monitoring metrics to CloudWatch Logs. Must be specified if monitoring_interval is non-zero"
+  default     = "30"
+  type = string
+}
+variable "performance_insights_retention_period" {
+  description = "The amount of time in days to retain Performance Insights data. Valid values are 7, 731 (2 years) or a multiple of 31"
+  default     = 7
+  type = number
+}
+variable "create_db_option_group" {
+  description = "Create a database option group"
+  default     = false
+  type = bool
+}
+variable "create_db_parameter_group" {
+  description = "Whether to create a database parameter group"
+  default     = true
+  type = bool
+}
+variable "backup_retention_period" {
+  description = "The days to retain backups for"
+  default     = 7
+  type = number
+}
+
+variable "family" {
+  description = "The family of the DB parameter group"
+  default     = "postgres15"
+  type = string
+}
+
